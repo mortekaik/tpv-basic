29ff6ec368166cd314e3c5eb1ac7204cdccd4fc5
mortekaik <astral.eskimo@gmail.com>
2019-08-08 20:09:14 +0300

    added some js to team section 2

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
-	// const socialsOpenBtn = document.querySelector('.socials__button--plus');
-	// const socialsList = document.querySelector('.socials__list');
-	// const personSocials = document.querySelector('.person__socials');
 
-	$('.person__socials').on('click', '.socials__button--plus', function () {
+	$('.person__socials').on('click', '.socials__button--plus', function(e) {
+		
+		const socialsList = $(this).siblings('.socials__list');
 		$(this).hide();
-		$('.socials__list').removeClass('socials__list--hidden').addClass('socials__list--showed');
+		// socialsList.removeClass('socials__list--hidden').fadeIn(1000);
+		socialsList.fadeIn(1000).css('display', 'flex');
+
 	});
+
 });
